05c3bc394db8aa62b2196bc04a9fffed4c6f1cb3
Vlad <VladDaImpaler@gmail.com>
2020-05-31 23:36:57 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -16,10 +16,7 @@ class Student
     SQL
     query_results = DB[:conn].execute(query)
     query_results.map do |row|
-      student = Student.new
-      student.id = row[0]
-      student.name = row[1]
-      student.grade = row[2]
+      new_from_db(row)
     end
 
     # retrieve all the rows from the "Students" database
@@ -30,16 +27,47 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE name = ? LIMIT 1;
     SQL
-    results = DB[:conn].execute(sql, name).first
-    student = self.new
-    student.id = results[0]
-    student.name = results[1]
-    student.grade = results[2]
-    student
+    query_result = DB[:conn].execute(sql, name).first
+    new_from_db(query_result)
     # find the student in the database given a name
     # return a new instance of the Student class
   end
   
+  def self.all_students_in_grade_9
+    sql_query = <<-SQL
+      SELECT * FROM students WHERE grade = 9;
+    SQL
+    query_results = DB[:conn].execute(sql_query)
+    query_results.map {|row| new_from_db(row)}
+  end
+  
+  def self.students_below_12th_grade
+    sql_query = <<-SQL
+      SELECT * FROM students WHERE grade < 12;
+    SQL
+    query_results = DB[:conn].execute(sql_query)
+    query_results.map {|row| new_from_db(row)}
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql_query = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT ?
+    SQL
+    query_results = DB[:conn].execute(sql_query, num)
+    query_results.map {|row| new_from_db(row)}
+  end
+
+  def self.first_student_in_grade_10
+    first_X_students_in_grade_10(1).first
+  end
+
+  def self.all_students_in_grade_X(num)
+    sql_query = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+    query_results = DB[:conn].execute(sql_query, num)
+    query_results.map {|row| new_from_db(row)}
+  end
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
