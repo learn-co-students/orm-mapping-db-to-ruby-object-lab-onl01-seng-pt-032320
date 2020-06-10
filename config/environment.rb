259c0f8c9333261b4279883551842486480ecc06
arnie b <arnold.bwaila@gmail.com>
2020-06-10 19:57:27 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-require 'sqllite3'
+require 'sqlite3'
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
           SELECT * FROM students
           SQL
     DB[:conn].execute(sql).collect do |row|
-      Song.new_from_db(row)
+      Student.new_from_db(row)
     end
   end
 
@@ -25,30 +25,62 @@ class Student
           LIMIT 1
           SQL
     DB[:conn].execute(sql,name).collect do |row|
-      Song.new_from_db(row)
+      Student.new_from_db(row)
     end.first
   end
   
-  def all_students_in_grade_9
+  def self.all_students_in_grade_9
     sql = <<-SQL
           SELECT * FROM students 
-          WHERE students.grade = '9th'
+          WHERE students.grade = '9'
           SQL
     DB[:conn].execute(sql).collect do |row|
-      Song.new_from_db(row)
+      Student.new_from_db(row)
     end
   end
   
-  def students_below_12th_grade
+  def self.students_below_12th_grade
     sql = <<-SQL
           SELECT * FROM students 
-          WHERE students.grade = '9th'
+          WHERE students.grade < '12'
           SQL
     DB[:conn].execute(sql).collect do |row|
-      Song.new_from_db(row)
+      Student.new_from_db(row)
     end
   end
-    
+  
+  def self.first_X_students_in_grade_10(num) 
+    sql = <<-SQL
+          SELECT * FROM students 
+          WHERE students.grade = '10'
+          LIMIT ?
+          SQL
+    DB[:conn].execute(sql,num).collect do |row|
+      Student.new_from_db(row)
+    end
+  end
+  
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+          SELECT * FROM students 
+          WHERE students.grade = '10'
+          LIMIT 1
+          SQL
+    DB[:conn].execute(sql).collect do |row|
+      Student.new_from_db(row)
+    end.first
+  end
+  
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+          SELECT * FROM students 
+          WHERE students.grade = ?
+          SQL
+    DB[:conn].execute(sql,grade).collect do |row|
+      Student.new_from_db(row)
+    end
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
