ea57af3e1483750174eb7166b8c73cd1ab24d7e1
arnie b <arnold.bwaila@gmail.com>
2020-06-10 19:35:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,19 +2,53 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+          SELECT * FROM students
+          SQL
+    DB[:conn].execute(sql).collect do |row|
+      Song.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+   sql = <<-SQL
+          SELECT * FROM students 
+          WHERE students.name = ?
+          LIMIT 1
+          SQL
+    DB[:conn].execute(sql,name).collect do |row|
+      Song.new_from_db(row)
+    end.first
   end
   
+  def all_students_in_grade_9
+    sql = <<-SQL
+          SELECT * FROM students 
+          WHERE students.grade = '9th'
+          SQL
+    DB[:conn].execute(sql).collect do |row|
+      Song.new_from_db(row)
+    end
+  end
+  
+  def students_below_12th_grade
+    sql = <<-SQL
+          SELECT * FROM students 
+          WHERE students.grade = '9th'
+          SQL
+    DB[:conn].execute(sql).collect do |row|
+      Song.new_from_db(row)
+    end
+  end
+    
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
